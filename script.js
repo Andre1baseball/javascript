9ed1954d572ac10534393b431e47af805f7c13cf
Andre1baseball <cullen.andre17@csj-rpi.org>
2015-09-17 09:34:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changeImage() { 
   var image = document.getElementById('myImage');
   if (image.src.match("bulbon")) {
-    image.src = "image/pic_bulboff.gif"
+    image.src = "images/pic_bulboff.gif"
     
   } else { 
-    image.src = "image/pic_bulbon.gif";
+    image.src = "images/pic_bulbon.gif";
   }
 }
